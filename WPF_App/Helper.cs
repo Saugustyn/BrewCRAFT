995d8834e3ecbf4546ad8555911a8f58ff5d0b76
Sebastian Augustyn <Sebastian.Augustyn@microsoft.wsei.edu.pl>
2022-07-06 20:05:59 +0200

    Refactoring of connection logic

---
+++ WPF_App/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_App
+{
+    internal class Helper
+    {
+        public static string connection = @"Data Source=LAPTOP-A0MV0IO4;Initial Catalog=Magazyn;Integrated Security=True";
+    }
+}
